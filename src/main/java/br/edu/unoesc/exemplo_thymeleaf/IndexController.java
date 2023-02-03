bfb8bb4fbf9dc59ac88ea387a3c1c9487e68613f
WendlingNathan <natritwen@gmail.com>
2023-02-03 20:18:40 -0300

    Mudança no nome do produto

--- src/main/java/br/edu/unoesc/exemplo_thymeleaf/IndexController.java
+++ src/main/java/br/edu/unoesc/exemplo_thymeleaf/IndexController.java
@@ -17,7 +17,7 @@ public class IndexController {
 		model.addAttribute("mensagem", "informação armazenada no modelo");
 		
 		Produto p1 = new Produto("Mesa", 10, new BigDecimal("3.33"));
-		Produto p2 = new Produto("Cadeira", 20, new BigDecimal("33.33"));
+		Produto p2 = new Produto("TV", 20, new BigDecimal("33.33"));
 		
 		List<Produto> produtos = new ArrayList<Produto>();
 		produtos.add(p1);
